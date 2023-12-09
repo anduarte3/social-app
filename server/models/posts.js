10e8ef2b92a08917f5f1f27fc8562f4f8dedd61f
andre <andreqt8@gmail.com>
2023-12-09 11:09:48 +0000

    fixed typo

--- server/models/posts.js
+++ server/models/posts.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const PostSchema = new mongoose.Schema({
+const PostSchema = new Schema({
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     content: { type: String, required: true },
     timestamp: { type: Date, default: Date.now },
-    // comments: [{
-    //   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    //   content: { type: String, required: true },
-    //   timestamp: { type: Date, default: Date.now }
-    // }],
-    // likes: [{
-    //   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    //   timestamp: { type: Date, default: Date.now }
-    // }]
+    comments: [{
+      user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      content: { type: String, required: true },
+      timestamp: { type: Date, default: Date.now }
+    }],
+    likes: [{
+      user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    }]
   });
 
 module.exports = mongoose.model("Post", PostSchema);
