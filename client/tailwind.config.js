7c2a57c33bd7686945b1e4ea4a5ca9ddf3150918
andre <andreqt8@gmail.com>
2024-08-26 21:41:03 +0100

    added tailwind css

---
+++ client/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', '../public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      'xs': '320px',
+      // => @media (min-width: 320px) { ... }
+
+      'vs': '475px',
+      // => @media (min-width: 475px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
