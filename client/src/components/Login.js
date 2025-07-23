e3204c0d1e942527ba2a2804a382817d913844c3
anduarte3 <andreqt8@gmail.com>
2025-07-23 13:38:26 +0100

    removed comments

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import user from '../img/user-icon.png'
-import password from '../img/password-icon.png'
 import profile from '../img/profile.png'
 import padlock from '../img/padlock.png'
 
