3a622cb6c5faf54f29de6d16e524af234d89344c
anduarte3 <andreqt8@gmail.com>
2025-07-27 12:40:22 +0100

    ready to deploy

--- client/src/api/DeletePostAPI.js
+++ client/src/api/DeletePostAPI.js
@@ -4,7 +4,7 @@ async function DeletePostAPI(postId) {
      try {
         const token = localStorage.getItem('token');
         // ${process.env.REACT_APP_LOCAL_URL} or ${process.env.REACT_APP_BACKEND_URL}
-        const response = await fetch(`${process.env.REACT_APP_LOCAL_URL}/api/post/${postId}/delete`, {
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/post/${postId}/delete`, {
             method: 'DELETE',
             credentials: 'include',
             headers: {
